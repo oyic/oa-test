2faf24e1d208c0626d42099daad5e2f161538740
oyic <oyic@outlook.com>
2021-07-10 08:39:11 +0800

    final commit for OA Test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-conssole.log('Hello')
+import "./scss/app.scss"
+import logo from "./images/oa-logo.png"
+
+let img = document.createElement('img');
+img.src = logo;
+
+document.querySelector('.logo-holder').appendChild(img)
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,61 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
+let mode = "development"
+let target = "web"
+if (process.env.NODE_ENV === 'production') {
+    mode = "production"
+    target = "browserslist"
+}
 module.exports = {
-    entry: [path.resolve(__dirname, 'src/index.js')],
-    module: devtool: false,
-    mode: 'development'
+    output: {
+        assetModuleFilename: "images/[name][ext][query]",
+    },
+    module: {
+        rules: [
+
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                exclude: /node_modules/,
+                type: "asset/resource"
+            },
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                exclude: /node_modules/,
+                use: [
+
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: "",
+                        }
+                    },
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+        ]
+    },
+    plugins: [new MiniCssExtractPlugin()],
+    target: target,
+    devtool: "source-map",
+    mode: mode,
+
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+    },
+
+    stats: {
+        children: true,
+    }
 }
